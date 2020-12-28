a35ef3ba9e79720b9dbc3453d043d08c53a43ce9
giorgia220523 <cocisgiorgia@gmail.com>
2020-12-28 19:06:48 +0200

    Laborator 11 - Done

--- Controllers/CustomersGrpcController.cs
+++ Controllers/CustomersGrpcController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Grpc.Net.Client;
-using GrpcCustomerService;
+using GrpcCustomersService;
 
 
 namespace Cocis_Giorgia_Lab2.Controllers
@@ -23,6 +23,7 @@ namespace Cocis_Giorgia_Lab2.Controllers
             CustomerList cust = client.GetAll(new Empty());
             return View(cust);
         }
+
         public IActionResult Create()
         {
             return View();
@@ -37,6 +38,94 @@ namespace Cocis_Giorgia_Lab2.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(customer);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(CustomerId customerId)
+        {
+            if (customerId == null)
+            {
+                return NotFound();
+            }
+
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Customer cust = client.Get(customerId);
+               
+            return View(cust);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(CustomerId customerId)
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Customer cust = client.Get(customerId);
+
+            if (cust == null)
+            {
+                return NotFound();
+            }
+           return View(cust);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(Customer customer)
+        {
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerId customerId = new CustomerId
+            {
+                Id = customer.CustomerId
+            };
+            Customer cust = client.Get(customerId);
+
+            if (cust == null)
+            {
+                return NotFound();
+            }
+
+            cust.Name = customer.Name;
+            cust.Adress = customer.Adress;
+            cust.Birthdate = customer.Birthdate;
+            client.Update(cust);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(CustomerId customerId)
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Customer cust = client.Get(customerId);
+
+            if (cust == null)
+            {
+                return NotFound();
+            }
+            return View(cust);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Customer customer)
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            CustomerId customerId = new CustomerId
+            {
+                Id = customer.CustomerId
+            };
+            
+            client.Delete(customerId);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
